1cf87a5f03f7a274c241d09ba549935c6b913c71
keyem4251 <kisk.emura@gmail.com>
2022-06-15 21:54:59 +0900

    tuple struct

--- chapter4/src/main.rs
+++ chapter4/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     fn_4_1();
+    fn_4_2();
 }
 
 fn fn_4_1() {
@@ -9,4 +10,31 @@ fn fn_4_1() {
     println!("{}", st1);
     // sliceで取り出すにはリファレンスにする
     println!("{}", &st1[0..6]);
+}
+
+fn fn_4_2() {
+    struct Person {
+        name: String,
+        age: u8,
+    }
+
+    let taro = Person{
+        name: "taro".to_string(),
+        age: 10,
+    };
+
+    let jiro = Person {
+        name: "jiro".to_string(),
+        ..taro
+    };
+    println!("name: {}, age: {}", jiro.name, jiro.age);
+
+    // タプル構造体
+    struct Celsius(f64); // 摂氏温度
+    struct Kelvin(f64); // 絶対温度
+
+    let temp_celsius = Celsius(10.0);
+    let temp_kelvin = Kelvin(10.0);
+
+    println!("celsius: {}, kelvin: {}", temp_celsius.0, temp_kelvin.0);
 }
