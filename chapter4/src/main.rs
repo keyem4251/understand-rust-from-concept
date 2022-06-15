83772290ff23df5249dc2df3ce5432455ea39aa0
keyem4251 <kisk.emura@gmail.com>
2022-06-15 21:39:53 +0900

    add string

--- chapter4/src/main.rs
+++ chapter4/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    fn_4_1();
+}
+
+fn fn_4_1() {
+    // &strからStringに変換して文字列を操作する
+    let mut st1 = "Hello".to_string();
+    st1.push_str(", World");
+    println!("{}", st1);
+    // sliceで取り出すにはリファレンスにする
+    println!("{}", &st1[0..6]);
 }
