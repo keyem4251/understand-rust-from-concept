6b41349cf61f29c6e25a83c2f7181a970a1aa279
keyem4251 <kisk.emura@gmail.com>
2022-06-19 20:57:33 +0900

    div zero

--- chapter4/src/fn_4_5.rs
+++ chapter4/src/fn_4_5.rs
@@ -1,3 +1,19 @@
 pub fn fn_4_5() {
-    println!("4_5");
+    fn_ex_print_result(fn_ex_div_result(10, 5));
+    fn_ex_print_result(fn_ex_div_result(10, 0));
+}
+
+fn fn_ex_div_result(x: i32, y: i32) -> Result<i32, &'static str> {
+    if y == 0 {
+        Err("div by zero")
+    } else {
+        Ok(x / y)
+    }
+}
+
+fn fn_ex_print_result<T: std::fmt::Display, E: std::fmt::Display>(ans: Result<T, E>) {
+    match ans {
+        Ok(res) => println!("{}", res),
+        Err(str) => println!("{}", str),
+    }
 }
