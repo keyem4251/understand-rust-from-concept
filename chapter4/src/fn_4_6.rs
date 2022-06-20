8592a50e7f1d75b880ea4542e3757090e1cb2f93
keyem4251 <kisk.emura@gmail.com>
2022-06-20 16:10:07 +0900

    json

--- chapter4/src/fn_4_6.rs
+++ chapter4/src/fn_4_6.rs
@@ -1,3 +1,38 @@
+use serde_json::json;
+
 pub fn fn_4_6() {
-    print!("");
+    fn_json_sample();
+}
+
+fn fn_json_sample() {
+    // JSONの文字列をValue型として読み込む
+    let hanako = json!({
+        "name": "hanako",
+        "age": 8,
+        "favorites": {
+            "food": ["apple", "melon"],
+        }
+    });
+
+    // Value型の中身を見てみる
+    println!("hanako debug: {:?}", hanako);
+
+    // Value型は[]を使ってアクセスできる
+    // 文字列はダブルクォーテーションマーク付きで返される
+    println!("hanako[\"name\"]: {}", hanako["name"]);
+
+    // 文字列はダブルクォーテーションマークなしで参照するには&str
+    println!("hanako[\"name\"].as_str().unwrap(): {}", hanako["name"].as_str().unwrap());
+
+    let mut taro = json!({});
+    taro["name"] = json!("taro");
+    taro["age"] = json!(10);
+    taro["favorites"] = json!({
+        "food": ["cake"],
+    });
+
+    let mut members = json!({});
+    members["members"] = json!([taro, hanako]);
+
+    println!("JSON: {}", members.to_string());
 }
