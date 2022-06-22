f165030e819293a68563caf09c3073e6dcb78472
keyem4251 <kisk.emura@gmail.com>
2022-06-22 21:27:27 +0900

    recursive enum

--- chapter4/src/fn_4_7.rs
+++ chapter4/src/fn_4_7.rs
@@ -1,3 +1,16 @@
 pub fn fn_4_7() {
-    
+    //  メモリへの値の配置
+    let boxed = Box::new(1);
+    let val = *boxed;
+    println!("val = {}", val);
+    println!("boxed = {}", boxed);
+
+    let recursive_enum = RecursiveEnum::Val(Box::new(RecursiveEnum::Null));
+    print!("{:?}", recursive_enum);
+}
+
+#[derive(Debug)]
+enum RecursiveEnum {
+    Val(Box<RecursiveEnum>),
+    Null,
 }
