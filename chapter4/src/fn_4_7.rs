3ebb0ea69fc3a2f39da57060326f5acf5318a4cb
keyem4251 <kisk.emura@gmail.com>
2022-06-23 21:36:38 +0900

    static and const

--- chapter4/src/fn_4_7.rs
+++ chapter4/src/fn_4_7.rs
@@ -1,3 +1,6 @@
+static BUFFSIZE: usize = 1024;
+static OFFSET: usize = 15;
+
 pub fn fn_4_7() {
     //  メモリへの値の配置
     let boxed = Box::new(1);
@@ -6,11 +9,23 @@ pub fn fn_4_7() {
     println!("boxed = {}", boxed);
 
     let recursive_enum = RecursiveEnum::Val(Box::new(RecursiveEnum::Null));
-    print!("{:?}", recursive_enum);
+    println!("{:?}", recursive_enum);
+
+    add_static();
+    
+    let offset_ref = &OFFSET;
+    
+    println!("buff size = {}", BUFFSIZE);
+    println!("offset ref = {}", offset_ref);
 }
 
 #[derive(Debug)]
 enum RecursiveEnum {
     Val(Box<RecursiveEnum>),
     Null,
+}
+
+fn add_static() {
+    const INCREMENT: usize = 2;
+    println!("increment = {}", INCREMENT);
 }
