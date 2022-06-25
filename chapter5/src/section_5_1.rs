ff6c8551534748131a855b8ac2aac38274fc9389
keyem4251 <kisk.emura@gmail.com>
2022-06-25 15:29:11 +0900

    add line trait

--- chapter5/src/section_5_1.rs
+++ chapter5/src/section_5_1.rs
@@ -4,16 +4,29 @@ pub fn fn_5_1() {
         height: 2.0,
     };
     println!("rect area = {}", area(&rect));
-
+    println!("rect length = {}", length(&rect));
+    
     let tria = RightTriangle {
         width: 1.0,
         height: 2.0,
     };
     println!("tria area = {}", area(&tria));
+    println!("tria length = {}", length(&tria));
+
+    let line = Line {
+        length: 1.0,
+    };
+    // println!("line area = {}", area(&line));
+    println!("line length = {}", length(&line));
+
 }
 
 trait CalcArea {
     fn calc_area(&self) -> f64;
+}
+
+trait CalcLength {
+    fn calc_length(&self) -> f64;
 }
 
 struct Rectangle {
@@ -24,6 +37,12 @@ struct Rectangle {
 impl CalcArea for Rectangle {
     fn calc_area(&self) -> f64 {
         self.width * self.height
+    }
+}
+
+impl CalcLength for Rectangle {
+    fn calc_length(&self) -> f64 {
+        (self.width + self.height) * 2.0
     }
 }
 
@@ -38,6 +57,26 @@ impl CalcArea for RightTriangle {
     }
 }
 
+impl CalcLength for RightTriangle {
+    fn calc_length(&self) -> f64 {
+        self.width + self.height + (self.width.powi(2) + self.height.powi(2)).sqrt()
+    }
+}
+
 fn area<T: CalcArea>(x: &T) -> f64 {
     x.calc_area()
+}
+
+struct Line {
+    length: f64,
+}
+
+impl CalcLength for Line {
+    fn calc_length(&self) -> f64 {
+        self.length
+    }
+}
+
+fn length<T: CalcLength>(x: &T) -> f64 {
+    x.calc_length()
 }
