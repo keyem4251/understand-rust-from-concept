4fd4b15fbb936a3d82709345130642764ac0328f
keyem4251 <kisk.emura@gmail.com>
2022-06-26 21:08:32 +0900

    add new section

--- chapter5/src/lib.rs
+++ chapter5/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod section_5_1;
 pub mod section_5_2;
+pub mod section_5_3;
--- chapter5/src/main.rs
+++ chapter5/src/main.rs
@@ -1,7 +1,9 @@
 use chapter5::section_5_1::fn_5_1;
 use chapter5::section_5_2::fn_5_2;
+use chapter5::section_5_3::fn_5_3;
 
 fn main() {
     fn_5_1();
     fn_5_2();
+    fn_5_3();
 }
