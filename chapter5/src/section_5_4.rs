a41ce767a0ae202d161847d692b25a60edeae33a
keyem4251 <kisk.emura@gmail.com>
2022-06-30 21:52:23 +0900

    add speaker

--- chapter5/src/section_5_4.rs
+++ chapter5/src/section_5_4.rs
@@ -1,6 +1,16 @@
 pub fn fn_5_4() {
     // 関連型
     // 独立な型パラメータと従属する型パラメータをトレイトに組み込む
+
+    // 継承
+    let en = EnglishPerson;
+    let sp = SpanishPerson;
+
+    say_hello_general(&en);
+    say_hello_general(&sp);
+
+    say_thankyou_general(&en);
+    say_thankyou_general(&sp);
 }
 
 // TとSが同じサイズ(i32, u32)
@@ -34,4 +44,47 @@ impl IAbs for i32 {
             (-self) as <Self as IAbs>::Output
         }
     }
+}
+
+trait SayHello {
+    fn say_hello(&self);
+}
+
+trait SayThankyou {
+    fn say_thankyou(&self);
+}
+
+struct EnglishPerson;
+struct SpanishPerson;
+
+impl SayHello for EnglishPerson {
+    fn say_hello(&self) {
+        println!("Hello");
+    }
+}
+
+impl SayThankyou for EnglishPerson {
+    fn say_thankyou(&self) {
+        println!("Thank you");
+    }
+}
+
+impl SayHello for SpanishPerson {
+    fn say_hello(&self) {
+        println!("Hola");
+    }
+}
+
+impl SayThankyou for SpanishPerson {
+    fn say_thankyou(&self) {
+        println!("Gracias");
+    }
+}
+
+fn say_hello_general<T: SayHello>(speaker: &T) {
+    speaker.say_hello();
+}
+
+fn say_thankyou_general<T: SayThankyou>(speaker: &T) {
+    speaker.say_thankyou();
 }
