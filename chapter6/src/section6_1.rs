ecd60014a1d547e44d49b61ed21746c8f8d3f7b8
keyem4251 <kisk.emura@gmail.com>
2022-07-02 21:07:41 +0900

    rust file io

--- chapter6/src/section6_1.rs
+++ chapter6/src/section6_1.rs
@@ -1,3 +1,40 @@
+use std::fs::File;
+use std::io::Read;
+
+const BUFSIZE: usize = 1024;
+
 pub fn fn_6_1() {
-    
+    match list_6_1() {
+        Ok(ok) => println!("OK: {:?}", ok),
+        Err(err) => println!("Err: {:?}", err),
+    };
+}
+
+fn list_6_1() -> std::io::Result<()> {
+    let mut f = File::open("input.txt")?;
+    let mut buf = [0_u8; BUFSIZE];
+
+    let mut lines: Vec<String> = Vec::new();
+    let mut linebuf = String::new();
+
+    loop {
+        let read_size = f.read(&mut buf)?;
+
+        if read_size == 0 {
+            break;
+        }
+
+        for cc in  &buf[..read_size]{
+            if *cc == b'\n' {
+                lines.push(linebuf);
+                linebuf = String::new();
+            } else {
+                linebuf.push(*cc as char);
+            }
+        }
+    }
+
+    println!("{:?}", lines);
+
+    Ok(())
 }
