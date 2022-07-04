28189c7610a276fdabdaf63609356b7c0c12d7ff
keyem4251 <kisk.emura@gmail.com>
2022-07-04 21:58:39 +0900

    add bmp

--- chapter6/src/section6_1.rs
+++ chapter6/src/section6_1.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Cursor};
+use byteorder::{LittleEndian, ReadBytesExt};
 use crate::line_read::get_lines;
 
 const BUFSIZE: usize = 1024;
@@ -75,4 +76,41 @@ fn list_6_3() -> std::io::Result<()> {
     println!("{:?}", lines);
 
     Ok(())
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug)]
+struct BmpFileHeader {
+    bfType: [u8; 2],
+    bfSize: u32,
+    bfReserved1: u16,
+    bfReserved2: u16,
+    bfOffBits: u32,
+}
+
+#[allow(non_snake_case)]
+impl BmpFileHeader {
+    fn parse_file_header(buf: &[u8]) -> std::io::Result<BmpFileHeader> {
+        let mut cur = Cursor::new(buf);
+        let mut bfType = [0_u8; 2];
+
+        for cc in &mut bfType {
+            *cc = cur.read_u8()?;
+        }
+
+        let bfSize = cur.read_u32::<LittleEndian>()?;
+        let bfReserved1 = cur.read_u16::<LittleEndian>()?;
+        let bfReserved2 = cur.read_u16::<LittleEndian>()?;
+        let bfOffBits = cur.read_u32::<LittleEndian>()?;
+
+        Ok(
+            BmpFileHeader {
+                bfType,
+                bfSize,
+                bfReserved1,
+                bfReserved2,
+                bfOffBits,
+            }
+        )
+    }
 }
