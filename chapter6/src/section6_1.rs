07b1dd9b5d867d8891296401fc34c6daef43b439
keyem4251 <kisk.emura@gmail.com>
2022-07-04 21:29:24 +0900

    read end

--- chapter6/src/section6_1.rs
+++ chapter6/src/section6_1.rs
@@ -8,6 +8,10 @@ pub fn fn_6_1() {
         Ok(ok) => println!("OK: {:?}", ok),
         Err(err) => println!("Err: {:?}", err),
     };
+    match list_6_2() {
+        Ok(ok) => println!("OK: {:?}", ok),
+        Err(err) => println!("Err: {:?}", err),
+    };    
 }
 
 fn list_6_1() -> std::io::Result<()> {
@@ -31,6 +35,29 @@ fn list_6_1() -> std::io::Result<()> {
             } else {
                 linebuf.push(*cc as char);
             }
+        }
+    }
+
+    println!("{:?}", lines);
+
+    Ok(())
+}
+
+fn list_6_2() -> std::io::Result<()> {
+    let mut f = File::open("input.txt")?;
+
+    let mut lines = Vec::new();
+    let mut linebuf = String::new();
+
+    let mut buf = Vec::new();
+    let read_size = f.read_to_end(&mut buf)?;
+
+    for cc in &buf[..read_size] {
+        if *cc == b'\n' {
+            lines.push(linebuf);
+            linebuf = String::new();
+        } else {
+            linebuf.push(*cc as char);
         }
     }
 
