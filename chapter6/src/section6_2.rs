be17325b33610511656bfd2ba798adc680455e7a
keyem4251 <kisk.emura@gmail.com>
2022-07-14 21:20:45 +0900

    add tcp listener

--- chapter6/src/section6_2.rs
+++ chapter6/src/section6_2.rs
@@ -1,1 +1,18 @@
-pub fn fn_6_2() {}
+use std::net::TcpListener;
+
+use crate::line_read::get_lines;
+
+pub fn fn_6_2() {
+    list_6_1().unwrap();
+}
+
+fn list_6_1() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:3210")?;
+
+    for stream in listener.incoming() {
+        let lines_vec = get_lines(stream?);
+        println!("{:?}", lines_vec);
+    }
+
+    Ok(())
+}
