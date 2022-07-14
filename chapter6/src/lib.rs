451a5795a0137606b3516afebe15f898c9dd7b0d
keyem4251 <kisk.emura@gmail.com>
2022-07-14 21:13:58 +0900

    add section 6 2

--- chapter6/src/lib.rs
+++ chapter6/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod line_read;
 pub mod section6_1;
+pub mod section6_2;
--- chapter6/src/main.rs
+++ chapter6/src/main.rs
@@ -1,5 +1,6 @@
-use chapter6::section6_1::fn_6_1;
+use chapter6::{section6_1::fn_6_1, section6_2::fn_6_2};
 
 fn main() {
     fn_6_1();
+    fn_6_2();
 }
