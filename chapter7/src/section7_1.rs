b01c9a57cab732900b42822be976889f77b995e3
keyem4251 <kisk.emura@gmail.com>
2022-07-16 16:50:29 +0900

    case 2

--- chapter7/src/section7_1.rs
+++ chapter7/src/section7_1.rs
@@ -1,5 +1,6 @@
 pub fn section7_1() {
     list7_1();
+    list7_2();
 }
 
 fn list7_1() {
@@ -15,4 +16,19 @@ fn list7_1() {
     // 所有権が移動しているためエラー
     // let x = vv[2];
     // println!("{}", x);
+}
+
+fn list7_2() {
+    let vv = vec![1, 2, 3, 4];
+    let mut iter = (&vv).iter();
+
+    let x = iter.next().unwrap();
+    println!("{}", x);
+
+    let x = iter.next().unwrap();
+    println!("{}", x);
+
+    // 参照のためエラーにならない
+    let x = vv[2];
+    println!("{}", x);
 }
