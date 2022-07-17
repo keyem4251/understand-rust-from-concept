6adf12c08b775925f180d60df15986e3fdf4b33e
keyem4251 <kisk.emura@gmail.com>
2022-07-17 21:27:22 +0900

    parttern match

--- chapter7/src/section7_3.rs
+++ chapter7/src/section7_3.rs
@@ -1,2 +1,23 @@
 pub fn section7_3() {
+    list7_1();
+}
+
+fn list7_1() {
+    let v = Vec::new();
+    let v = insert(2,&v);
+    println!("{:?}", v);
+}
+
+fn insert(x: i32, xs: &[i32]) -> Vec<i32> {
+    match xs {
+        // ベクタの最初の用途をy、その他の要素（..）を@でysとして取り出す
+        [y, ys @ ..] => {
+            if x <= *y {
+                [&[x][..], xs].concat()
+            } else {
+                [&[*y][..], &insert(x, ys)].concat()
+            }
+        }
+        [] => vec![x],
+    }
 }
