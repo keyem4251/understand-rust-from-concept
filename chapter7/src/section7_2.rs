0ecf97b0760ba570f5434b43572d53a8ffefe917
keyem4251 <kisk.emura@gmail.com>
2022-07-17 21:07:07 +0900

    add vec

--- chapter7/src/section7_2.rs
+++ chapter7/src/section7_2.rs
@@ -1,5 +1,8 @@
 pub fn section7_2() {
     println!("{}", fact(3));
+
+    let vv = vec![0, 1, 2, 3];
+    println!("{:?}", list7_1(vv));
 }
 
 fn fact(n: u32) -> u32 {
@@ -8,4 +11,14 @@ fn fact(n: u32) -> u32 {
     } else {
         n * fact(n - 1)
     }
+}
+
+fn list7_1(vv: Vec<i32>) -> Vec<i32> {
+    let mut ret = Vec::new();
+
+    for ee in vv {
+        ret.push(ee + 1);
+    }
+
+    ret 
 }
