e197daa1054ee06f571c32ae533a977b0383dbc5
keyem4251 <kisk.emura@gmail.com>
2022-07-18 14:12:39 +0900

    spawn

--- chapter8/src/section8_1.rs
+++ chapter8/src/section8_1.rs
@@ -1,3 +1,15 @@
+use std::thread::spawn;
+
 pub fn section8_1() {
-    
+    list8_1();
+}
+
+fn list8_1() {
+    let mut v_threads = Vec::new();
+    for i in 0..10 {
+        let thread = spawn(move || println!("{}", i));
+        v_threads.push(thread);
+    }
+    let x: Vec<()> = v_threads.into_iter().map(|th| th.join().unwrap()).collect();
+    println!("{:?}", x);
 }
