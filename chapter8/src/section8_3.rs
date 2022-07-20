ddd8f34d6297cad81fcd2c12c307b57851b223b7
keyem4251 <kisk.emura@gmail.com>
2022-07-20 21:30:02 +0900

    async spawn

--- chapter8/src/section8_3.rs
+++ chapter8/src/section8_3.rs
@@ -1,6 +1,7 @@
 pub fn section8_3() {
     fn8_1();
     fn8_2();
+    fn8_3();
 }
 
 async fn sum_func(n: usize) -> usize {
@@ -26,4 +27,24 @@ fn fn8_2() {
         sum_func(100).await;
         sum_func(20).await;
     });
+}
+
+async fn print_number(n: usize) -> usize {
+    for ii in 0..2 {
+        tokio::time::sleep(tokio::time::Duration::from_millis(10)).await;
+        println!("{} from {}", ii, n);
+    }
+    n
+}
+
+#[tokio::main]
+async fn fn8_3() {
+    let mut handler_vec = Vec::new();
+    (1..=3).for_each(|ii| {
+        handler_vec.push(tokio::spawn(print_number(ii)));
+    });
+
+    let ret = futures::future::join_all(handler_vec).await;
+    let retval = ret.into_iter().map(|r| r.unwrap()).collect::<Vec<usize>>();
+    println!("{:?}", retval);
 }
